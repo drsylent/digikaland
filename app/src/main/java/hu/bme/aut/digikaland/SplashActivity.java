d22f31374b40aafc1aba1bf5ccba7d04a0bd6d2d
Csendes Dávid <sylence96@gmail.com>
2018-02-21 10:45:31 +0100

    Splash preparation

--- app/src/main/java/hu/bme/aut/digikaland/SplashActivity.java
+++ app/src/main/java/hu/bme/aut/digikaland/SplashActivity.java
@@ -9,5 +9,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        // itt lesz az adatbazis betoltese, es annak eldontese
+        // melyik kepernyo keruljon betoltesre
+        try{
+            Thread.sleep(1000);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        // elinditasa a megfelelo activitynek
+
     }
 }
