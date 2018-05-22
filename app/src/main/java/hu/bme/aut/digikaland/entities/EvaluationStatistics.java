528668f16a13b933b5916f88011b86fb33a86fb3
Csendes Dávid <sylence96@gmail.com>
2018-05-22 18:09:53 +0200

    Refactor and documentation EvaluationStatistics

--- app/src/main/java/hu/bme/aut/digikaland/entities/EvaluationStatistics.java
+++ app/src/main/java/hu/bme/aut/digikaland/entities/EvaluationStatistics.java
@@ -2,14 +2,16 @@ package hu.bme.aut.digikaland.entities;
 
 import java.io.Serializable;
 
-// TODO: elterjeszteni máshova is
+/**
+ * Egy osztály, mely összefoglalja, egy állomáson mi a kiértékelés állapota.
+ */
 public class EvaluationStatistics implements Serializable {
     public int evaluated;
     public int done;
     public int all;
-    public EvaluationStatistics(int ev, int don, int notst){
+    public EvaluationStatistics(int ev, int don, int all){
         evaluated = ev;
         done = don;
-        all = notst;
+        this.all = all;
     }
 }
