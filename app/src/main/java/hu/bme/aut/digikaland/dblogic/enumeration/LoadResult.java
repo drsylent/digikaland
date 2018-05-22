0aab0dc260487eda5e536a5c36cf9c43ed5a46e8
Csendes Dávid <sylence96@gmail.com>
2018-05-22 17:38:14 +0200

    Refactor and documentation LoadResult

--- app/src/main/java/hu/bme/aut/digikaland/dblogic/enumeration/LoadResult.java
+++ app/src/main/java/hu/bme/aut/digikaland/dblogic/enumeration/LoadResult.java
@@ -1,12 +1,23 @@
 package hu.bme.aut.digikaland.dblogic.enumeration;
 
 /**
- * Created by Sylent on 2018. 05. 10..
+ * A játék állapota micsoda egy felhasználó szemszögéből - mi lett a fő betöltés eredménye.
  */
-
 public enum LoadResult {
+    /**
+     * A verseny épp elindul / még nem kezdődött el.
+     */
     Starting,
+    /**
+     * A verseny már megy, és úton vannak a versenyzők.
+     */
     Running,
+    /**
+     * A kliens jelenleg egy állomáson feladatot old.
+     */
     Station,
+    /**
+     * A verseny a végénél tart.
+     */
     Ending
 }
